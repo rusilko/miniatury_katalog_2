cc92e2c5db9258a8db78570b49841024f71b01cf
tomek-rusilko <tomek.rusilko@gmail.com>
2012-03-13 14:39:33 +0100

    minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,5 +23,6 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
 
   validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 
 end
