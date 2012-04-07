cb625dcff78eedfc1fbbb2d702511a7c8649325a
tomek-rusilko <tomek.rusilko@gmail.com>
2012-04-07 17:40:36 +0200

    Finally done with user sign-up

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -20,12 +20,19 @@ describe "UserPages" do
   end
 
   describe "signup" do
-    
+    let(:submit) { "Create my account" }
     before { visit signup_path }
 
     describe "with invalid information" do
-      it "shoudl not create a new user" do
-        expect { click_button "Create my account" }.not_to change(User, :count)
+
+      describe "error messages" do
+        before { click_button submit }
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
+      
+      it "should not create a new user" do
+        expect { click_button submit }.not_to change(User, :count)
       end
     end
 
@@ -39,8 +46,16 @@ describe "UserPages" do
 
       it "should create a new user" do
         expect do
-          click_button "Create my account"
+          click_button submit
         end.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com')}
+ 
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
 
